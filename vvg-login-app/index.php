88b2c48b0bb139fe5d23f98c50bb9f75bcf191f3
Davor Lozic <warriorkitty@warriorkitty.com>
2017-12-09 08:47:39 +0100

    Add search to vvg-login-app

--- vvg-login-app/index.php
+++ vvg-login-app/index.php
@@ -12,12 +12,31 @@ echo $header;
 
 // get users
 $sql = 'SELECT id, email, first_name, last_name, is_active FROM users';
+
+$isSearch = isset($_GET['q']);
+
+if($isSearch) { $sql .= ' WHERE email LIKE :email'; }
+
 $stmt = $db->prepare($sql);
+
+if($isSearch) {
+  $search = "%{$_GET['q']}%";
+  $stmt->bindParam(':email', $search);
+}
+
 $stmt->execute();
 $users = $stmt->fetchAll();
 ?>
 
 <div class="container content-wrapper">
+  <form method="GET" action="index.php">
+    <div class="form-group">
+      <input name="q"
+             value="<?= isset($_GET['q']) ? $_GET['q'] : '' ?>"
+             type="text" class="form-control" placeholder="Search for ..."
+             autofocus="autofocus">
+    </div>
+  </form>
   <a href="add.php"><span class="oi oi-plus"></span></a>
   <table class="table table-hover">
     <thead>
@@ -38,11 +57,15 @@ $users = $stmt->fetchAll();
       <td><?= $user['last_name'] ?></td>
       <td>
         <a href="edit.php?id=<?= $user['id'] ?>"><span class="oi oi-pencil"></span></a>
-        <a href="delete.php?id=<?= $user['id'] ?>"><span class="oi oi-delete"></span></a>
-        <?php if($user['is_active'] === '1'): ?>
-          <a href="change_activation.php?id=<?= $user['id'] ?>"><span class="oi oi-ban"></span></a>
-        <?php else: ?>
-          <a href="change_activation.php?id=<?= $user['id'] ?>"><span class="oi oi-check"></span></a>
+
+        <?php $isMyId = $user['id'] === $_SESSION['user']['id']; ?>
+        <?php if(!$isMyId): ?>
+          <a href="delete.php?id=<?= $user['id'] ?>"><span class="oi oi-delete"></span></a>
+          <?php if($user['is_active'] !== '1'): ?>
+            <a href="change_activation.php?id=<?= $user['id'] ?>"><span class="oi oi-check"></span></a>
+          <?php else: ?>
+            <a href="change_activation.php?id=<?= $user['id'] ?>"><span class="oi oi-ban"></span></a>
+          <?php endif; ?>
         <?php endif; ?>
       </td>
     </tr>
