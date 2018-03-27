02d45ed3501ddcac90c12e6af444753265d1b06d
Davor Lozic <warriorkitty@warriorkitty.com>
2018-03-27 18:25:06 +0200

    Add cursor exercies to pl-sql

---
+++ pl-sql/exercises/04_cursors.sql
@@ -0,0 +1,15 @@
+-- 1. Napiši program koji će pomoću kursora dohvatiti sve korisnike čije ime
+-- počinje samoglasnikom.
+
+-- 2. Napiši program koji će pomoću kursora dohvatiti sve korisnike koji imaju
+-- parametar jezik postavljen na 'Hrvatski'.
+
+-- 3. Napiši program koji će pomoću kursora dohvatiti sve korisnike koji su
+-- direktori (tablica 'company_positions'), a nisu administratori (tablica 'roles').
+
+-- 4. Napiši program koji će pomoću kursora ispisati sve korisnike kojima broj
+-- mobitela završava brojem 222. Izvan kursora, ispisati korisnika samo ako mu
+-- ime počinje slovom A, a prezime slovom M.
+
+-- 5. Napiši program koji će pomoću kursora ispisati puno ime i prezime (u jednoj koloni)
+-- te njegovu poziciju. Ispis je potrebno napraviti po prezimenu uzlazno.
