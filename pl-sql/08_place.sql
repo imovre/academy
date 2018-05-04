003169ec6de336af355d98904f43fcdb26a01209
Davor Lozic <warriorkitty@warriorkitty.com>
2018-05-04 10:38:48 +0200

    Update pl-sql

--- pl-sql/08_place.sql
+++ pl-sql/08_place.sql
@@ -79,3 +79,80 @@ BEGIN
 END;
 
 /
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(porezni_razred(5000));
+END;
+
+/
+
+CREATE OR REPLACE FUNCTION oporezivi_dohodak(gross_income DECIMAL, ukupna_olaksica DECIMAL, bruto_doprinosi DECIMAL)
+RETURN DECIMAL IS
+    result DECIMAL;
+BEGIN
+    result := gross_income - ukupna_olaksica - bruto_doprinosi;
+    return result;
+END;
+
+/
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(oporezivi_dohodak(10000, 3800, 2000));
+END;
+
+/
+
+CREATE OR REPLACE FUNCTION porezni_razred(gross_income DECIMAL)
+RETURN DECIMAL IS
+    result DECIMAL;
+BEGIN
+    SELECT MIN(vrijednost) INTO result FROM porezni_razredi WHERE gross_income <= vrijednost_do;
+    return result;
+END;
+
+/
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(porezni_razred(10000));
+END;
+
+/
+
+CREATE OR REPLACE FUNCTION ukupni_porez(oporezivi_dohodak DECIMAL, porezni_razred DECIMAL)
+RETURN NUMBER IS
+    result DECIMAL(10, 2);
+BEGIN
+    result := oporezivi_dohodak * porezni_razred;
+    return result;
+END;
+
+/
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(ukupni_porez(4200, 0.24));
+END;
+
+/
+
+CREATE OR REPLACE FUNCTION ukupni_prirez(porez DECIMAL, user_id NUMBER)
+RETURN NUMBER IS
+    result DECIMAL(10, 2);
+    postotak DECIMAL(10, 2) := 0;
+BEGIN
+    SELECT surtax
+    INTO postotak
+    FROM cities
+        INNER JOIN users
+            ON cities.id = users.city_id
+    WHERE users.id = user_id;
+    result := porez * postotak;
+    return result;
+END;
+
+/
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(ukupni_prirez(1008, 1));
+END;
+
+/
